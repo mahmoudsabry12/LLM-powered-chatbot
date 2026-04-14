0d6bbce47db0dcdcd0c2937033f761888900dfd4
mahmoudsabry12 <mahmoudsabry.udacity@gmail.com>
2026-04-14 12:40:01 +0200

    add simple middleware to check cicd sec file

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,16 @@ import meetingRoutes from "./modules/meeting/meeting.route";
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "default-src 'self'");
+  res.setHeader("X-Frame-Options", "DENY");
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.setHeader("Strict-Transport-Security", "max-age=31536000");
+
+  next();
+});
+
 app.use("/api/meetings", meetingRoutes);
 
 app.get("/", (_, res) => {
